509ead44cce9d44f7aa8f15bfda15562dc63714a
0Arafsir0 <arafsirarafat@gmail.com>
2025-10-16 02:36:52 +0600

    Insctructions Added in README file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,29 +4,29 @@
 <div class="max-w-3xl mx-auto mt-10 bg-white p-8 rounded shadow text-center">
     <h1 class="text-3xl font-bold text-gray-800 mb-4">About Developer</h1>
     <div class="flex flex-col items-center">
-        <img src="{{ asset('images/priyam.jpg') }}" alt="Developer Avatar"
+        <img src="" alt="Developer Avatar"
              class="w-32 h-32 rounded-full shadow mb-4 border-4 border-indigo-500">
         <h2 class="text-2xl font-semibold text-indigo-700">Priyam Sharma</h2>
-        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono">0222220005101018</span></p>
+        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono"></span></p>
    
 
 
-<img src="{{ asset('images/ayas.jpg') }}"alt="Developer Avatar"
+<img src=""alt="Developer Avatar"
              class="w-32 h-32 rounded-full shadow mb-4 border-4 border-indigo-500">
-        <h2 class="text-2xl font-semibold text-indigo-700">MD Aias</h2>
-        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono">0222220005101041</span></p>
+        <h2 class="text-2xl font-semibold text-indigo-700"></h2>
+        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono"></span></p>
 
 
-<img src="{{ asset('images/arafat.jpg') }}" alt="Developer Avatar"
+<img src="" alt="Developer Avatar"
              class="w-32 h-32 rounded-full shadow mb-4 border-4 border-indigo-500">
         <h2 class="text-2xl font-semibold text-indigo-700">Mohammad Arafat Siraj</h2>
-        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono">0222220005101030</span></p>
+        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono"></span></p>
 
         
-<img src="{{ asset('images/omi.jpg') }}" alt="Developer Avatar"
+<img src="" alt="Developer Avatar"
              class="w-32 h-32 rounded-full shadow mb-4 border-4 border-indigo-500">
-        <h2 class="text-2xl font-semibold text-indigo-700">Abir Ahamed Omi</h2>
-        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono">0222220005101021</span></p>
+        <h2 class="text-2xl font-semibold text-indigo-700"></h2>
+        <p class="text-gray-600 text-sm mt-1">ID: <span class="font-mono"></span></p>
 
 
         <p class="mt-6 text-gray-700 leading-relaxed">
